cf5fe67dfd490c27ff6b2af2079ac2ce686a1b83
d1mk0v <79874763588@ya.ru>
2023-06-03 13:02:05 +0500

    Hogwarts

---
+++ src/Hogwarts.java
@@ -0,0 +1,30 @@
+public class Hogwarts {
+    private String name;
+    private int magic;
+    private int transgression;
+
+    public Hogwarts(String name, int magic, int transgression) {
+        this.name = name;
+        this.magic = magic;
+        this.transgression = transgression;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getMagic() {
+        return magic;
+    }
+
+    public int getTransgression() {
+        return transgression;
+    }
+
+    public void setMagic(int magic) {
+        this.magic = magic;
+    }
+
+    public void setTransgression(int transgression) {
+        this.transgression = transgression;
+    }
+}
