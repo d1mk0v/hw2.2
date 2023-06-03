04eeb2782264e7e35e674a753cf8fea769df9007
d1mk0v <79874763588@ya.ru>
2023-06-03 13:54:01 +0500

    Slytherin

---
+++ src/Slytherin.java
@@ -0,0 +1,56 @@
+public class Slytherin extends Hogwarts{
+    private int cunning;
+    private int determination;
+    private int ambition;
+    private int resourcefulness;
+    private int lustForPower;
+
+    public Slytherin(String name, int magic, int transgression, int cunning, int determination, int ambition, int resourcefulness, int lustForPower) {
+        super(name, magic, transgression);
+        this.cunning = cunning;
+        this.determination = determination;
+        this.ambition = ambition;
+        this.resourcefulness = resourcefulness;
+        this.lustForPower = lustForPower;
+    }
+
+    public int getCunning() {
+        return cunning;
+    }
+
+    public int getDetermination() {
+        return determination;
+    }
+
+    public int getAmbition() {
+        return ambition;
+    }
+
+    public int getResourcefulness() {
+        return resourcefulness;
+    }
+
+    public int getLustForPower() {
+        return lustForPower;
+    }
+
+    public void setCunning(int cunning) {
+        this.cunning = cunning;
+    }
+
+    public void setDetermination(int determination) {
+        this.determination = determination;
+    }
+
+    public void setAmbition(int ambition) {
+        this.ambition = ambition;
+    }
+
+    public void setResourcefulness(int resourcefulness) {
+        this.resourcefulness = resourcefulness;
+    }
+
+    public void setLustForPower(int lustForPower) {
+        this.lustForPower = lustForPower;
+    }
+}
