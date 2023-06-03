d2cef441f9de3ce2c4e26309f09e0cc81cbc0eac
d1mk0v <79874763588@ya.ru>
2023-06-03 13:47:42 +0500

    Ravenclaw

---
+++ src/Ravenclaw.java
@@ -0,0 +1,46 @@
+public class Ravenclaw extends Hogwarts {
+    private int intellect;
+    private int wisdom;
+    private int wit;
+    private int creation;
+
+    public Ravenclaw(String name, int magic, int transgression, int intellect, int wisdom, int wit, int creation) {
+        super(name, magic, transgression);
+        this.intellect = intellect;
+        this.wisdom = wisdom;
+        this.wit = wit;
+        this.creation = creation;
+    }
+
+    public int getIntellect() {
+        return intellect;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public int getWit() {
+        return wit;
+    }
+
+    public int getCreation() {
+        return creation;
+    }
+
+    public void setIntellect(int intellect) {
+        this.intellect = intellect;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public void setWit(int wit) {
+        this.wit = wit;
+    }
+
+    public void setCreation(int creation) {
+        this.creation = creation;
+    }
+}
